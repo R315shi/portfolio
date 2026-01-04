4f245a0aa67e66bfb7aceaf1f8ba305340e36628
Reis Cakaj <reiscakaj123@gmail.com>
2026-01-04 18:16:54 +0100

    4th commit

--- src/components/MainComponents.jsx
+++ src/components/MainComponents.jsx
@@ -5,18 +5,23 @@ const MainComponents = () => {
     
   return (
     <div className='maindiv'>
+
         <div className='firstdiv'
         data-aos="zoom-in">
-          Test
+          <div className='text1'>
+            <h1>Reis Cakaj</h1><br/>
+            <h3>Frontend / Full-Stack* Student Developer.<br/>Building modern web applications with React</h3>
+            <div className='wrap'>
+              <a href="#projects" className='link1'><h5>View Projects</h5></a>
+            </div>
+          </div>
+        </div> 
+        <div className='seconddiv'>
+           <div className='text1'>
+            <h1>About Me</h1><br/>
+            <h3>Student that has lost his will to live bruh what is thiss ;-;<br/>Building modern web applications with React</h3>
+          </div>         
         </div>
-        <div className='firstdiv'
-        data-aos="zoom-in">
-          Test
-        </div>
-        <div className='firstdiv'
-        data-aos="zoom-in">
-          Test
-        </div>     
     </div>
   )
 }
